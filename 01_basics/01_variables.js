b79010d5e46aa20d35a14736dcec2b65b3bcb3d5
Nawazishahmad <68150741+Nawazishahmad@users.noreply.github.com>
2024-02-20 16:03:05 +0000

    data types & variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445533
+let accountEmail = "Ahmadnawazish.3157@gmial.com"
+var accountPassword = "12345"
+accountCity="Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "Nawazishshan31@gmail.com"
+accountPassword = "2112121"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as a newer version 
+
+// alert("hello") // we are using node js not browser
+
+// code readability should be high //
+console.log(3+3)
+
+console.log("nawazish")
+
+let name = "Nawazish"
+let age = 18
+let isloggedin = true
+let state;
+
+
+// data types// 
+// number 
+// bigint
+// string ""
+// boolen => true/false
+// null => standalone value
+// undefine
+// symbol
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)    // object
+
