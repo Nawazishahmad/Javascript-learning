42fe8b39d6da4964573498955de9010c7e856eb7
Nawazishahmad <68150741+Nawazishahmad@users.noreply.github.com>
2024-02-22 14:20:29 +0000

    conversion operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,12 +1,12 @@
-/*let score ="33"
+let score ="33"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber= Number (score)
-console.log(typeof valueInNumber)
+// let valueInNumber= Number (score)
+// console.log(typeof valueInNumber)
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 // "33"=>33 (Conversion is possilble)
 // "33abc" => NaN
@@ -28,4 +28,57 @@ let RandomNumber= 33
 
 let stringNumber = String(RandomNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+//************* operations ***************
+
+let value=4
+
+let negativevalue=-value
+
+// console.log(negativevalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%4);
+
+let str1="Nawazish"
+let str2 = " Ahmad"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1" + 3);
+// console.log(1 + "3"); 
+// console.log("1" + "3"); 
+
+// console.log("1" + 3 + 2);
+// console.log(1 + 3 + "2"); 
+
+// if string is in first the whole console.log will be treated as string. in the above example output will be 132.
+
+// if string is in last then the first two values will get added and then string will be added. in the above exmaple output will be 42.
+
+
+// console.log(3 + 4 * 5 % 3);
+
+// 1. Multiply 4 by 5, which is 20.
+// 2. Divide 20 by 3 and find the remainder, which is 2.
+// 3. Add 3 to the remainder, which gives 5.
+// 4. Print the result, which is 5.
+
+console.log(+true);
+
+let num1, num2, num3
+
+let number= num1 = num2 = num3 =2+2
+console.log(number);
+
+let gamecounter = 100
+
+gamecounter++;
+console.log(gamecounter);
