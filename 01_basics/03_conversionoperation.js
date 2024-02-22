cef1bca31725debced36f650f400334a0813cff0
Nawazishahmad <68150741+Nawazishahmad@users.noreply.github.com>
2024-02-22 13:01:47 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+/*let score ="33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber= Number (score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+// "33"=>33 (Conversion is possilble)
+// "33abc" => NaN
+
+// true=> 1; false =0
+
+// null => 0 */
+//let isLoggedIn = "Nawazish"
+
+//let booleanisLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Nawazish" => true
+
+let RandomNumber= 33
+
+let stringNumber = String(RandomNumber)
+
+console.log(typeof stringNumber);
